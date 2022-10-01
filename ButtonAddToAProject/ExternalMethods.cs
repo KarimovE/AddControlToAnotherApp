0854b3855ccfb88768882166bc9c321a1a2338c5
Emil Karimov <91150600+KarimovE@users.noreply.github.com>
2022-10-01 12:32:59 +0400

    Project added

---
+++ ButtonAddToAProject/ExternalMethods.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.TeamFoundation.Common.Internal;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class ExternalMethods
+{
+
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string ClassN, string WindN);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+
+}
+
---
+++ ButtonAddToAProject/Helper.cs
@@ -0,0 +1,30 @@
+﻿public class Helper
+{
+    public static void AddButtonToApp()
+    {
+        var mainWindow = ExternalMethods.FindWindow("AdImpApplicationFrame", null);
+
+        if (mainWindow != IntPtr.Zero)
+        {
+            var child = ExternalMethods.FindWindowEx(mainWindow, IntPtr.Zero, "ATL:6549C390", null);
+
+            if (child != IntPtr.Zero)
+            {
+                var button = new Button { Text = "Button", Left = 150, Top = 10, Width = 70, Height = 20 };
+                button.Click += (o, args) => { Console.WriteLine("Clicked successfully"); };
+
+                ExternalMethods.SetParent(button.Handle, child);
+            }
+        }
+
+        //var currentProc = Process.GetCurrentProcess();
+        //string name = currentProc.ProcessName;
+
+     
+        Console.WriteLine("test");
+    }
+
+
+
+}
+
